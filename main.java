ab0d4048ace2542918bf50ddc226e3680e28043d
Mike <michealehadera048@gmail.com>
2023-07-24 22:19:14 -0500

    made  some changes

--- main.java
+++ main.java
@@ -2,5 +2,8 @@ class Main {
 	public static void main(String[] args) {
 		
 		System.out.println(" This is my day one java file in my git repository");
+
+
+		System.out.println("This is a conflict check");
 	}
 }
