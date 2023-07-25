38d8d2ac27087d2d20a9551941a456922c5687e4
Micheale Teweldemedhin <112814896+WediHade@users.noreply.github.com>
2023-07-24 22:16:41 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ class Main {
 	public static void main(String[] args) {
 		
 		System.out.println(" This is my day one java file in my git repository");
+		System.out.println("This is a conflict check");
 	}
 }
